af8a63209169ce96550be189f809b462c49ca2ad
june jepkirui <jepkiruijune@gmail.com>
2024-06-28 16:07:06 +0300

    Done With Assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+let name= "Ruto"
+function introduction(name){
+    return `Hi, my name is ${name}.`;
+    
+}
+ function introductionWithLanguage(name, language){
+        return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+    }
+  function introductionWithLanguageOptional(name, language = "JavaScript"){
+     return `Hi, my name is ${name} and I am learning to program in ${language}.`; } caches
+
+   
